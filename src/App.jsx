a7cf1d0af7e5f8730c84ed74cfc0519e48f7787e
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-27 21:50:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default Navbar
